1c7a962ca24a5283c7536e61afb51f462128826e
John Yenter-Briars <johnyenterbriars@protonmail.com>
2026-01-02 11:06:44 -0600

    register

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use actix_web::{App, HttpServer, middleware::Logger};
-use tvc::{DOMAIN, PORT, routes::commands::{power_off, power_on, toggle_mute, volume_down, volume_up}};
+use tvc::{DOMAIN, PORT, routes::commands::{active_source, power_off, power_on, register_playback, toggle_mute, volume_down, volume_up}};
 
 
 #[actix_web::main]
@@ -10,11 +10,13 @@ async fn main() -> std::io::Result<()> {
 
     HttpServer::new(|| {
         App::new()
+            .service(register_playback)
             .service(power_off)
             .service(power_on)
             .service(volume_up)
             .service(volume_down)
             .service(toggle_mute)
+            .service(active_source)
             .wrap(Logger::new("%a %{User-Agent}i %r %s %U %{Content-Type}i"))
     })
     .bind((DOMAIN, PORT))?
